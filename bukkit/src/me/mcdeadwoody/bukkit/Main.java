3fbeaa02e66d689a079b382ca43707de356775f6
Mcdeadwoody <mcdeadwoody@gmail.com>
2014-11-30 22:20:55 +0100

    test

--- bukkit/src/me/mcdeadwoody/bukkit/Main.java
+++ bukkit/src/me/mcdeadwoody/bukkit/Main.java
@@ -1,6 +1,12 @@
 package me.mcdeadwoody.bukkit;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
 
 /**
  * Created by Tim on 30-11-2014.
@@ -9,14 +15,43 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin
 {
+
+
+    Logger logger = Bukkit.getLogger();
+    @Override
     public void onEnable()
     {
-        
+        for(int i=0;i<5;i++)
+        {
+            logger.info("Test plugin <Mcdeadwoody> starting..");
+        }
     }
+    @Override
     public void onDisable()
     {
-
+        logger.info("Test plugin <Mcdeadwoody> shutting down..");
     }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+    {
+        if(label.equalsIgnoreCase("fly"))
+        {
+            Player p = (Player) sender;
+            if(p.isFlying())
+            {
+                p.setFlying(true);
+            }else
+            {
+                p.setFlying(false);
+            }
+        }
+        return true;
+    }
+
+
+
+
+
 
 
 }
