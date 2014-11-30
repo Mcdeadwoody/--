4f6b1c42ffac48f03cd7609c019d294046878871
Mcdeadwoody <mcdeadwoody@gmail.com>
2014-11-30 23:10:34 +0100

    doublejump & basic code

--- bukkit/src/me/mcdeadwoody/bukkit/Main.java
+++ bukkit/src/me/mcdeadwoody/bukkit/Main.java
@@ -4,9 +4,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerVelocityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
@@ -22,46 +21,47 @@ public class Main extends JavaPlugin implements Listener
 
     Logger logger = Bukkit.getLogger();
     @Override
-    public void onEnable()
-    {
+    public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
-        for(int i=0;i<5;i++)
-        {
+        for(int i=0;i<5;i++) {
             logger.info("Test plugin <Mcdeadwoody> starting..");
         }
     }
     @Override
-    public void onDisable()
-    {
+    public void onDisable() {
         logger.info("Test plugin <Mcdeadwoody> shutting down..");
     }
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
     {
-        if(label.equalsIgnoreCase("fly"))
-        {
-            Player p = (Player) sender;
-            if(p.isFlying())
-            {
-                p.setFlying(true);
-            }else
-            {
-                p.setFlying(false);
-            }
-        }
         return true;
     }
 
-    @EventHandler
-    public void playerBlockBreakEvent(BlockBreakEvent e)
-    {
-        if(!e.getPlayer().isOp())
-        {
-            e.setCancelled(true);
+    // Minigame ideëen:
+
+    // double jump
+    public void doubleJump(PlayerVelocityEvent event) {
+        Player p = event.getPlayer();
+        int doubleJumpCount = 0;
+
+
+
+        if(p.isFlying()) {
+            if(p.isSneaking()) {
+                if (doubleJumpCount == 0) {
+
+                    //TODO setvelocity for the next jump, reset upon touching the ground, possible XP bar state indication.
+
+                    doubleJumpCount = 1;
+                }else
+                    if(doubleJumpCount == 1){
+                    return;
+                }
+
+
+            }
         }
     }
-
-
 
 
 
