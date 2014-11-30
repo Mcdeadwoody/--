953a41ffb3f9156bb391a7f868516901ae06ccb8
Mcdeadwoody <mcdeadwoody@gmail.com>
2014-11-30 22:10:24 +0100

    Initial commit

---
+++ bukkit/src/me/mcdeadwoody/bukkit/Main.java
@@ -0,0 +1,22 @@
+package me.mcdeadwoody.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Tim on 30-11-2014.
+ */
+
+
+public class Main extends JavaPlugin
+{
+    public void onEnable()
+    {
+        
+    }
+    public void onDisable()
+    {
+
+    }
+
+
+}
