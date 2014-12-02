03cb92fb44e3374b7caf996a085b7ee84d222bf9
Mcdeadwoody <mcdeadwoody@gmail.com>
2014-12-02 15:54:26 +0100

    Doublejump

--- bukkit/src/me/mcdeadwoody/bukkit/Main.java
+++ bukkit/src/me/mcdeadwoody/bukkit/Main.java
@@ -1,13 +1,17 @@
 package me.mcdeadwoody.bukkit;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 
@@ -30,27 +34,30 @@ public class Main extends JavaPlugin implements Listener
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
     {
+        if(label.equalsIgnoreCase("test")) {
+            Player p = (Player) sender;
+            p.sendMessage("test");
+        }
         return true;
     }
 
     // double jump
-    Object jumpState = null;
     @EventHandler
-    public void onInteract(PlayerInteractEvent e) {
-        if(e.getAction() == Action.PHYSICAL) {
-            if(e.getMaterial() == Material.AIR) {
-                if(jumpState == null) {
-                    e.getPlayer().setVelocity(new Vector(e.getPlayer().getVelocity().getBlockX(), 0.25, e.getPlayer().getVelocity().getBlockZ()));
-                    for(int i=0; i<5; i++) {
-                        e.getPlayer().sendMessage("vis");
-                    }
-                    jumpState = 1;
-                }else {
-                    e.setCancelled(true);
-                }
-            }
+    public void onPlayerToggleFlight(PlayerToggleFlightEvent e) {
+        if(e.getPlayer().getGameMode() != GameMode.CREATIVE) {
+            e.getPlayer().setVelocity(new Vector(e.getPlayer().getVelocity().getBlockX(), 1, e.getPlayer().getVelocity().getBlockZ()));
+            e.setCancelled(true);
         }
     }
+
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent e) {
+            if (e.getPlayer().getLocation().subtract(0, 1, 0).getBlock().getType() != Material.AIR && e.getPlayer().getGameMode() != GameMode.CREATIVE) {
+                e.getPlayer().setAllowFlight(true);
+            }
+    }
+
 
 
 
